f91254f67fb621216660e8e460f84a0851e45498
Michael <mdart86@hotmail.com>
2021-01-30 23:33:16 +1000

    additional code added to routes and controller for show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,12 @@ class BooksController < ApplicationController
     # The before action method runs any private helper methods before your action methods, you can also specify which action method you want to run this helper method against.
 
     before_action :read_books, only: [:index]
+    before_action :set_book, only: [:show]
 
     def index
+    end
+
+    def show
     end
 
     # private methods are helper methods
@@ -15,4 +19,14 @@ class BooksController < ApplicationController
     # Book.all is a class method on the model class (book) that is in the active record base class that we are inherting from and needs to be stored in an instance variable
     # performs a select * on the table associated with that model and returns all the data
     # this class method should have a singular name as we are calling a class method on the book model class
+
+    def set_book
+        if params[:id]
+            @book = Book.find(params[:id])
+        end 
+    end
+
+    # we are populating the singular instance variable (to match the prefix)
+    # we are using an if statement againsts the params of :id
+    # The above code is saying if we have an id we are going to set @book to be equal to book.find(params[:id])
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "books#index"
   get "/books", to: "books#index"
-
+  get "/books/:id", to: "books#show", as: "book"
 end
+# get methods gets the page from the server
+# the route with books/:id as your URL and the books#show with the prefix of books will show the data of a book to the user
