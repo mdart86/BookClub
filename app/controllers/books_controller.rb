78927867516601e059331d84d3fc390ab4bd0ea4
Michael <mdart86@hotmail.com>
2021-01-30 16:39:02 +1000

    updates to controller and index.html.erb added to views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,18 @@
 class BooksController < ApplicationController
+    # The before action method runs any private helper methods before your action methods, you can also specify which action method you want to run this helper method against.
+
+    before_action :read_books, only: [:index]
+
+    def index
+    end
+
+    # private methods are helper methods
+    private
+    def read_books
+        @books = Book.all
+    end
+
+    # Book.all is a class method on the model class (book) that is in the active record base class that we are inherting from and needs to be stored in an instance variable
+    # performs a select * on the table associated with that model and returns all the data
+    # this class method should have a singular name as we are calling a class method on the book model class
 end
