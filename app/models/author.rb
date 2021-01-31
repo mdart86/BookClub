51fc82f0774e0cf8e8b0fafdc3787ea3f9107253
Michael <mdart86@hotmail.com>
2021-01-31 10:48:00 +1000

    created partial book file and updated show view and added metatag

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,10 @@
 class Author < ApplicationRecord
     has_many :books
+
+    def name
+        return "#{first_name} #{last_name}"
+    end
+    # this instance method will get the first and last name from the author model class in the database. Rails magic means it doesn't need to refer to an instance variable
 end
+
+
